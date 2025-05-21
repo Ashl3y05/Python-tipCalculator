25b40a94f6764a4077543f28b3d6d34de8ec54b8
Ashley Panelo <118011180+Ashl3y05@users.noreply.github.com>
2025-05-22 04:15:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-#Variables
-name = input("What is your name?") #Equal sign is an assignment operator to assign a value to a variable
-print(name) #printing the value of the variable "name"
+#If the bill was $150.00, split between 5 people, with 12% tip.
 
-name = "I'll change your name to Donut" #changes the last value of the variable "name"
-print(name)
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
 
-#make variable names meaningful and descriptive as much as possible
-#use underscores,uppercase, or numbers to separate words in a variable name
-#DON'T use space when naming variable
-#DON'T use reserved words as variable names
-#DON'T use special characters in variable names
-#DON'T use numbers as the first character in a variable name
-#ALWAYS define variable names before using them
+print("Welcome to the Tip Calculator!")
+totalBill = float(input("Total Bill?:")) #getting the total bill and converting it to a float
+numberOfPayees = int(input("How Many are paying?:")) #getting the number of people paying and converting it to an integer
+tipPercentage = float(input("How much tip you want to give in percentage?")) #getting the tip percentage and converting it to a float
+tip = totalBill * (tipPercentage/100) #dividing the tip percentage by 100 to get the decimal value of the tip and multiplying it by the total bill to get the tip
+
+totalBill += tip #adding the tip to the total bill
+print(totalBill)
+billPerPreson= totalBill/numberOfPayees #dividing the total bill by the number of people paying to get the bill per person
+finalAmount= round(billPerPreson,2)
+finalAmount = "{:.2f}".format(billPerPreson) #formatting the bill per person to 2 decimal places"
+print(f"Individual payment: {finalAmount}")
